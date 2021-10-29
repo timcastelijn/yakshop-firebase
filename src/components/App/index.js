01584cac5780a643f193cd00d16884d9690c4468
timcastelijn <t.castelijn@hotmail.com>
2021-10-29 17:01:45 +0200

    fixed for windows

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -17,7 +17,7 @@ import HomePage from '../Home';
 import Quotes from '../Quotes';
 import Viewer from '../Viewer';
 import Library from '../Library';
-import Quote from '../Quote';
+// import Quote from '../Quote';
 import AboutPage from '../AboutPage';
 import AccountPage from '../Account';
 import AdminPage from '../Admin';
@@ -53,7 +53,7 @@ const App = () => (
           <Route path={'/millingstats'} component={MillingStats} />
           <Route path={'/timetracking'} component={TimeTrackingPage} />
           <Route path={'/timetracking/:code'} component={ResponseCode} />
-          <Route path={ROUTES.QUOTE} component={Quote} />
+          
           <Route path={ROUTES.ACCOUNT} component={AccountPage} />
           <Route path={ROUTES.ADMIN} component={AdminPage} />
         </Container>
